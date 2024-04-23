a64b0a55bb24abed95cc693b548690feef90e5aa
darkjoker1021 <alessandro.canistro06@gmail.com>
2024-04-23 19:55:18 +0200

    A lot of fixes

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ function hideShowIndice() {
 
 	if (indice.style.display === "none") {
 		indice.style.display = "block";
-		button.style.backgroundColor = "midnightblue"
+		button.style.backgroundColor = "midnightblue";
 	} else {
 		indice.style.display = "none";
-		button.style.backgroundColor = "dodgerblue"
+		button.style.backgroundColor = "dodgerblue";
 	}
+}
+
+function darkMode() {
+  var body = document.body;
+  var darkbtn = document.getElementById("darkbtn");
+
+  body.classList.toggle("dark-mode");
+
+  if (darkbtn.innerHTML === "Chiaro") {
+	darkbtn.style.backgroundColor = "midnightblue";
+  	darkbtn.innerHTML = "Scuro";
+  } else {
+	darkbtn.style.backgroundColor = "dodgerblue";
+	darkbtn.innerHTML = "Chiaro";
+  }
 }
