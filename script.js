9940d11082d01e8bc794ae317c6be67fd5ff48ea
darkjoker1021 <alessandro.canistro06@gmail.com>
2024-04-26 22:37:11 +0200

    Add animation login sign-in

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 function darkMode() {
-  var body = document.body;
-  var darkbtn = document.getElementById("darkbtn");
+  let body = document.body;
+  let darkbtn = document.getElementById("darkbtn");
 
   body.classList.toggle("dark-mode");
 
   if (darkbtn.innerHTML === "Chiaro") {
-	darkbtn.style.backgroundColor = "midnightblue";
+	  darkbtn.style.backgroundColor = "midnightblue";
   	darkbtn.innerHTML = "Scuro";
   } else {
-	darkbtn.style.backgroundColor = "";		//Reset del colore di sfondo
-	darkbtn.innerHTML = "Chiaro";
+    darkbtn.style.backgroundColor = "";
+    darkbtn.innerHTML = "Chiaro";
   }
+}
+
+function showLogin() {
+    var loginDiv = document.getElementById("login");
+    var signinDiv = document.getElementById("signin");
+
+    loginDiv.classList.toggle("fadeindiv");
+    loginDiv.classList.remove("fadeoutdiv");
+
+    signinDiv.classList.toggle("fadeoutdiv");
+    signinDiv.classList.remove("fadeindiv");
+
+    signinDiv.style.display = "none";
+    loginDiv.style.display = "";
+}
+
+function showSignin() {
+    var loginDiv = document.getElementById("login");
+    var signinDiv = document.getElementById("signin");
+
+    signinDiv.classList.toggle("fadeindiv");
+    signinDiv.classList.remove("fadeoutdiv");
+
+    loginDiv.classList.toggle("fadeoutdiv");
+    loginDiv.classList.remove("fadeindiv");
+
+    loginDiv.style.display = "none";
+    signinDiv.style.display = "";
 }
